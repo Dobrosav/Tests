29eb1a08643633c885b4fe57dffb08e5a2fb7c98
Dobrosav Vlaskovic <vlaskovic.dodo@gmail.com>
2021-05-15 20:23:32 +0200

    Finished Junit testing

--- AutomatedTest/src/videosTest/TestJunit.java
+++ AutomatedTest/src/videosTest/TestJunit.java
@@ -1,6 +1,9 @@
 package videosTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Scanner;
+
 import org.junit.*;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -32,9 +35,28 @@ public class TestJunit {
 		driver.get("https://classtranscribe-dev.ncsa.illinois.edu/video?id=696f4642-a2cb-4bb8-b9c9-f0af26e5677d");
 	}
 	
-	/*@Test
+	@Test
 	public void testVideoEntry() {
 		driver.get("https://classtranscribe-dev.ncsa.illinois.edu/video?id=696f4642-a2cb-4bb8-b9c9-f0af26e5677d");
-	}*/
-	
+	}
+	@Test
+	public void testPlayOrPaused() {
+		driver.findElement(By.id("play-btn")).click();
+	}
+	@Test
+	public void editFirstTitle() {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		String desiredText;
+		Scanner s=new Scanner(System.in);
+		desiredText=s.nextLine();
+		js.executeScript("document.getElementById('caption-line-textarea-0d01ddb7-057c-4e72-9e34-0f17943eb690').innerHTML="+ desiredText);
+	}
+	@Test
+	public void testPlay() {
+		driver.findElement(By.cssSelector("watch-ctrl-button")).click();
+	}
+	@Test
+	public void close() {
+		driver.close();
+	}
 }
